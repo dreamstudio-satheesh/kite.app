103b0832c353227b9b95d8973ad2bacec4e63ae8
dreamstudio-satheesh <4798470+dreamstudio-satheesh@users.noreply.github.com>
2025-05-22 10:48:02 +0530

    app

--- backend/resources/views/layouts/app.blade.php
+++ backend/resources/views/layouts/app.blade.php
@@ -42,7 +42,8 @@
     <script src="{{ asset('assets/libs/feather-icons/feather.min.js') }}"></script>
     <script src="{{ asset('assets/js/pages/plugins/lord-icon-2.1.0.js') }}"></script>
     <script src="{{ asset('assets/js/plugins.js') }}"></script>
-    <script src="{{ asset('assets/js/app.js') }}"></script>
+
+    {{-- <script src="{{ asset('assets/js/app.js') }}"></script> --}}
 
 
     @stack('scripts')
